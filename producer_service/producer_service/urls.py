13afd7f5c2f1264712362bf852e2ba2fd016089d
O-sama2004 <o.elbagory97@gmail.com>
2025-07-29 02:22:41 +0300

    added reciever app in consumer service

--- producer_service/producer_service/urls.py
+++ producer_service/producer_service/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sender.views import send_message
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('send/', send_message),  # 
 ]
--- producer_service/sender/views.py
+++ producer_service/sender/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from kafka import KafkaProducer
+import json
+
+producer = KafkaProducer(
+    bootstrap_servers='kafka:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+def send_message(request):
+    data = {"message": "Hello from producer!"}
+    producer.send('test-topic', value=data)
+    return JsonResponse({"status": "message sent"})
